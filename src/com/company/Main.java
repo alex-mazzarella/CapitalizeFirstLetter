f32def34fc70a4bde181b22745e42f79f0946bcf
alex-mazzarella <64137828+alex-mazzarella@users.noreply.github.com>
2020-05-21 15:52:47 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import acm.program.ConsoleProgram;
+
+/***Console Program - takes a String in Input and
+ * prints the string with first letter only uppercase .
+ * implement method isEnglishConsonant*/
+
+public class Main extends ConsoleProgram {
+
+    public static void main(String[] args) {
+
+        (new com.company.Main()).start();
+    }
+
+    public void run() {
+
+        String input=readLine("Insert string here: ");
+        println(capitalize(input));
+
+
+
+    }
+
+    private String capitalize(String input) {
+        String result = "";
+        result=result+input.charAt(0);
+        result=result.toUpperCase();
+        input=input.toLowerCase();
+        return result+input.substring(1);
+
+    }
+}
